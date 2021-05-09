3dc5564f49c895b2190ee8d2087127264bf4ffe4
zengpan15 <3198612440@qq.com>
2021-05-09 22:21:39 +0800

    web

---
+++ src/main/java/com/zp/dao/BookDao.java
@@ -0,0 +1,84 @@
+package com.zp.dao;
+
+import com.zp.javabean.Book;
+import com.zp.db.JDBCUtil;
+import java.sql.Date;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+public class BookDao {
+    public List<Book> selectAll(int pageNum, int pageSize) {
+        String sql = "select books.*, book_sort.name as sort " +
+                "from books, book_sort where " +
+                "books.sort_id=book_sort.id limit ?,?";
+
+        List<Book> books = new ArrayList<>();
+        try (ResultSet rs =
+                     JDBCUtil.getInstance().executeQueryRS(sql,
+                             new Object[]{(pageNum - 1) * pageSize,
+                                     pageSize})) {
+
+            while (rs.next()) {
+                Book book = new Book(rs.getInt("id") + "",
+                        rs.getString(
+                                "name"),
+                        rs.getString("author"),
+                        rs.getString("sort"),
+                        rs.getString("description"));
+                books.add(book);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return books;
+    }
+
+    public int count() {
+        String sql = "select count(*) as countNum from books";
+        try (ResultSet rs =
+                     JDBCUtil.getInstance().executeQueryRS(sql,
+                             new Object[]{})) {
+
+            while (rs.next()) {
+                int count = rs.getInt("countNum");
+                return count;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    public boolean selectStore(String username, String bookId) {
+        String sql1 = "select EXISTS( SELECT 1 from borrow_books " +
+                "where book_id=? and card_id=?) as store";
+        try (ResultSet rs =
+                     JDBCUtil.getInstance().executeQueryRS(sql1,
+                             new Object[]{
+                                     bookId, username
+                             });) {
+
+            while (rs.next()) {
+                return rs.getBoolean("store");
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public int insertStoreBook(String username, String bookId) {
+        String sql = "insert into borrow_books(book_id, card_id, " +
+                "borrow_date) values(?,?,?)";
+        int result = JDBCUtil.getInstance().executeUpdate(sql,
+                new Object[]{
+                        bookId, username,
+                        new Date(System.currentTimeMillis())
+                });
+        return result;
+    }
+}
